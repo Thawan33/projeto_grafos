b994f72429de404f642cd32d086c9c94b5a0d630
Thawan33 <thawanrodrigue@gmail.com>
2023-11-13 20:58:14 -0300

    1.0

--- matriz_adj.cpp
+++ matriz_adj.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+
+// Definindo códigos de cores ANSI
+#define RESET   "\033[0m"       /* Reset */
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLD    "\033[1m"       /* Bold */
+
+
 int** criar_matriz(int lin,int col){
     int **mat;
     mat = new int*[lin];
@@ -21,14 +36,16 @@ void preencher_matriz(int** mat,int lin,int col){
 void imprimir_matriz(int** mat, int lin, int col){
     cout << "  ";
     for(int i=0; i<col; i++){
-        cout << i << " ";
+        cout << BOLD << i << " ";
     }
+    cout << RESET;
     cout << endl;
     for(int i=0; i<lin; i++){
-        cout << i << " ";
+        cout << BOLD  << i << RESET << " ";
         for(int j=0; j<col; j++){
-            cout<<mat[i][j]<<" ";
+            cout<< YELLOW <<mat[i][j]<<" ";
         }
+        cout << RESET;
         cout << endl;
     }
 }
@@ -170,7 +187,7 @@ bool planar(int quant_vertices,int quant_arestas){
     while(resp != 's' && resp != 'n');
     //consertar para incluir na condição se a quantidade de vertices for menor ou igual a 2!!!!!
     if(resp == 'n'){
-        if(quant_arestas <= 3 * quant_vertices - 6){
+        if(quant_arestas <= 3 * quant_vertices - 6 || quant_vertices <= 2){
             return true;
         }else{
             return false;
